e521c5607b2c5b228784a2dd8a7c52bf52c04a3e
nedim <h.nedim@gmail.com>
2025-07-23 14:58:05 +0700

    add

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # neidiom.github.io
 
 https://neidiom.github.io/
+
+```
+ sudo apt-get update && sudo apt-get install fonts-noto-color-emoji
+ ```
--- build_pdf.sh
+++ build_pdf.sh
@@ -5,4 +5,7 @@ css_file="_site/assets/main.css"
 
 
 
-pandoc "${input_file}" -o "${output_file}" --pdf-engine=xelatex --css "${css_file}" -H pdf_style.tex
+temp_input_file="${input_file}.no_emoji"
+ruby -e 'File.open(ARGV[1], "w") { |out| File.foreach(ARGV[0]) { |line| out.puts line.encode("UTF-8", invalid: :replace, undef: :replace).gsub(/[^\x00-\x7F]/, "") } }' "${input_file}" "${temp_input_file}"
+pandoc --from=html "${temp_input_file}" -o "${output_file}" --pdf-engine=xelatex --css "${css_file}" -H pdf_style.tex
+rm "${temp_input_file}"
